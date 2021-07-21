6b1b63a6eb17d782dbcd8d6ff07a83ec0a88fd26
Nizami Alimamedov <nizamialimamedov@Nizamis-MacBook-Pro.local>
2021-07-21 23:58:04 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => {
+  const [color, setColor] = useState("grey");
+  const [fontSize, setFontSize] = useState(6);
+  const [opacity, setOpacity] = useState(0);
+
+  return (
+    <main>
+      <h6 style={{ fontSize: fontSize }}>Hello To Everyone</h6>
+      <div className="wrapper-button">
+        <button className="btn btn-primary" onClick={() => setColor("yellow")}>
+          Warning 😅
+        </button>
+        <button className="btn btn-primary" onClick={() => setColor("red")}>
+          Error 😡
+        </button>
+        <button className="btn btn-primary" onClick={() => setColor("green")}>
+          Success 😍
+        </button>
+      </div>
+      <div
+        className="plus-minus-icon"
+        onClick={() => setFontSize((f) => f - 2)}
+      >
+        -
+      </div>
+      <div
+        className="plus-minus-icon"
+        onClick={() => setFontSize((f) => f + 2)}
+      >
+        +
+      </div>
+      <div className="wrapper-square" style={{ backgroundColor: color }}></div>
+      <br />
+      <ul style={{ opacity: opacity }}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Categories</li>
+      </ul>
+      <button
+        className="btn btn-block btn-success"
+        onClick={() => {
+          setOpacity(1);
+        }}
+      >
+        Show
+      </button>
+      <button
+        className="btn btn-block btn-success"
+        onClick={() => {
+          setOpacity(0);
+        }}
+      >
+        Hide
+      </button>
+    </main>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
